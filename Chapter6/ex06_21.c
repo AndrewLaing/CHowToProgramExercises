91b8dd6114c8847f8c1d553cf5aa99f07a95401c
Andrew Laing <parisianconnections@gmail.com>
2017-09-05 11:13:40 +0100

    Update ex06_21.c

--- Chapter6/ex06_21.c
+++ Chapter6/ex06_21.c
@@ -35,66 +35,66 @@ int acceptAlternative( const char* );
 
 void printPass( int seatNum )
 {
-	printf("\n---------------------------------------\n");
-	if(seatNum < 5)
-	    printf("              FIRST CLASS\n");
-	else
-	    printf("             ECONOMY CLASS\n");
-	printf("---------------------------------------\n");
-	printf("Flight LH907\tBoarding\tSeat No\n");
-	printf("To Boston\t  9:10am\t%4d\n", seatNum+1);
-	printf("\nDeparture 9:40am at Gate 15\n");
-	printf("---------------------------------------\n\n");
+    printf("\n---------------------------------------\n");
+    if(seatNum < 5)
+        printf("              FIRST CLASS\n");
+    else
+        printf("             ECONOMY CLASS\n");
+    printf("---------------------------------------\n");
+    printf("Flight LH907\tBoarding\tSeat No\n");
+    printf("To Boston\t  9:10am\t%4d\n", seatNum+1);
+    printf("\nDeparture 9:40am at Gate 15\n");
+    printf("---------------------------------------\n\n");
 }
 
 
 /* Returns 1 if a seat was assigned else 0 */
 int assignFirstClassSeat(int seats[])
 {
-	int seatAssigned = 0;
-	int i;
+    int seatAssigned = 0;
+    int i;
 	
-	for(i=0; i<5; i++)
+    for(i=0; i<5; i++)
+    {
+	if(seats[i]==0)
 	{
-		if(seats[i]==0)
-		{
-			seats[i]=1;
-			seatAssigned = 1;
-			printPass( i );
-			break;
-		} 
-	}
+            seats[i]=1;
+            seatAssigned = 1;
+	    printPass( i );
+	    break;
+	} 
+    }
 	
-	return seatAssigned;	
+    return seatAssigned;	
 }
 
 
 /* Returns 1 if a seat was assigned else 0 */
 int assignEconomySeat(int seats[])
 {
-	int seatAssigned = 0;
-	int i;
+    int seatAssigned = 0;
+    int i;
 	
-	for(i=5; i<10; i++)
+    for(i=5; i<10; i++)
+    {
+	if(seats[i]==0)
 	{
-		if(seats[i]==0)
-		{
-			seats[i]=1;
-			seatAssigned = 1;
-			printPass( i );
-			break;
-		} 
-	}
+	    seats[i]=1;
+	    seatAssigned = 1;
+	    printPass( i );
+	    break;
+	} 
+    }
 	
-	return seatAssigned;	
+    return seatAssigned;	
 }
 
 
 int acceptAlternative(const char *menu)
 {
-	int choice;
-	printf(menu);
-	scanf("%d", &choice);
+    int choice;
+    printf(menu);
+    scanf("%d", &choice);
 	
     return choice==1;
 }
@@ -102,53 +102,50 @@ int acceptAlternative(const char *menu)
 
 int main()
 {
-	int seatsTaken = 0;
-	int choice;
-	int seatsAvailable[ MAXSEATS ] = { 0 };
+    int seatsTaken = 0;
+    int choice;
+    int seatsAvailable[ MAXSEATS ] = { 0 };
 	
-	while(seatsTaken < MAXSEATS )
+    while(seatsTaken < MAXSEATS )
     {
     	printf(MENU);
     	scanf("%d", &choice);
     	
     	if(choice==1)
     	{
-    		if(assignFirstClassSeat(seatsAvailable) == 0 )
-    		{
-    			if(acceptAlternative(MENU1) == 1) {
-    				assignEconomySeat(seatsAvailable); 
-    				seatsTaken++;
-				}
-				else {
-					printf("\nNext flight leaves in three hours!\n");
-				}
-			}
-			else {
-				seatsTaken++;
-			}
+            if(assignFirstClassSeat(seatsAvailable) == 0 )
+    	    {
+    		if(acceptAlternative(MENU1) == 1) {
+    		    assignEconomySeat(seatsAvailable); 
+    		    seatsTaken++;
 		}
+		else 
+		    printf("\nNext flight leaves in three hours!\n");
+	    }
+	    else {
+		seatsTaken++;
+	    }
+	}
     	else if(choice==2)
     	{
-    		if(assignEconomySeat(seatsAvailable) == 0 )
-    		{
-    			if(acceptAlternative(MENU2) == 1) {
-    				assignFirstClassSeat(seatsAvailable); 
-    				seatsTaken++;
-				}
-				else {
-					printf("\nNext flight leaves in three hours!\n");
-				}
-			}
-			else {
-				seatsTaken++;
-			}
+    	    if(assignEconomySeat(seatsAvailable) == 0 )
+    	    {
+    		if(acceptAlternative(MENU2) == 1) {
+    		    assignFirstClassSeat(seatsAvailable); 
+    		    seatsTaken++;
 		}
-		else {
-			printf("\nInvalid menu option!\n");	
+		else
+		    printf("\nNext flight leaves in three hours!\n");
 		}
-	}	
-	printf("\nFlight is now fully booked.\n");
+	    else {
+	        seatsTaken++;
+	    }
+	}
+	else {
+	    printf("\nInvalid menu option!\n");	
+	}
+    }	
+    printf("\nFlight is now fully booked.\n");
 	
     return 0;
 }
-
