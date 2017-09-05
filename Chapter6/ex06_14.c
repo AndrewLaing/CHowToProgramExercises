44e22665fca6235b103b7e276a38611beff4562c
Andrew Laing <parisianconnections@gmail.com>
2017-09-05 11:00:26 +0100

    Update ex06_14.c

--- Chapter6/ex06_14.c
+++ Chapter6/ex06_14.c
@@ -17,9 +17,9 @@ void printArray( const int[] );
 
 int main()
 {
-	int frequency[ 10 ] = {0};
-	int response[ SIZE ] =
-	    { 6,7,7,9,7,7,8,9,8,9,
+    int frequency[ 10 ] = {0};
+    int response[ SIZE ] =
+            { 6,7,7,9,7,7,8,9,8,9,
 	      7,8,9,5,9,8,7,8,7,8,
 	      6,7,8,9,3,9,8,7,8,7,
 	      7,8,9,8,9,8,9,7,8,9,
@@ -30,116 +30,115 @@ int main()
 	      7,4,4,2,5,3,8,7,5,6,
 	      4,5,6,1,6,5,7,8,7 };
 	
-	mean( response );
-	median( response );
-	mode( frequency, response );
+    mean( response );
+    median( response );
+    mode( frequency, response );
 	
     return 0;
 }
 
+
 void mean( const int answer[] )
 {
-	int j, total=0;
+    int j, total=0;
 	
-	printf("%s\n%s\n%s\n", "********", "  Mean", "********");
+    printf("%s\n%s\n%s\n", "********", "  Mean", "********");
 	
-	for(j=0; j<=SIZE-1; j++)
+    for(j=0; j<=SIZE-1; j++)
 	total += answer[ j ];
 	
     printf( "The mean is the average value of the data\n" 
-	        "items. The mean is equal to the total of\n"
-			"all of the data items divided by the number\n"
-			"of data items ( %d ). The mean for\n"
-			"this run is: %d / %d = %.4f\n\n",
-			SIZE, total, SIZE, (double)total/SIZE );
+            "items. The mean is equal to the total of\n"
+            "all of the data items divided by the number\n"
+            "of data items ( %d ). The mean for\n"
+	    "this run is: %d / %d = %.4f\n\n",
+	    SIZE, total, SIZE, (double)total/SIZE );
 }
 
 void median ( int answer[] )
 {
-	printf("\n%s\n%s\n%s\n%s", 
-	       "********", " Median", "********",
-	       "The unsorted array of response is"
-	);
+    printf("\n%s\n%s\n%s\n%s", 
+           "********", " Median", "********",
+           "The unsorted array of response is");
 	
-	printArray( answer );
-	bubbleSort( answer );
-	printf( "\n\nThe sorted array is" );
-	printArray( answer );
-	printf( "\n\nThe median is element %d of\n"
-	        "the sorted %d element array.\n"
-			"For this run the median is %d\n\n",
-			SIZE/2, SIZE, answer[ SIZE/2]  );
+    printArray( answer );
+    bubbleSort( answer );
+    printf( "\n\nThe sorted array is" );
+    printArray( answer );
+    printf( "\n\nThe median is element %d of\n"
+            "the sorted %d element array.\n"
+            "For this run the median is %d\n\n",
+    	    SIZE/2, SIZE, answer[ SIZE/2]  );
 }
 
 void mode( int freq[], const int answer[] )
 {
-	int rating, j, h, largest=0;
+    int rating, j, h, largest=0;
 	
-	printf("\n%s\n%s\n%s\n", 
-	       "********", "  Mode", "********");
+    printf("\n%s\n%s\n%s\n", 
+           "********", "  Mode", "********");
 	       
-	for(rating=1; rating<=9; rating++)
-	    freq[ rating ] = 0;
+    for(rating=1; rating<=9; rating++)
+        freq[ rating ] = 0;
 	
-	for(j=0; j<=SIZE-1; j++)
-	    ++freq[ answer[j] ];
+    for(j=0; j<=SIZE-1; j++)
+        ++freq[ answer[j] ];
 	    
-	printf( "%s%11s%19s\n%54s\n%54s\n",
-	        "Response", "Frequency", "Histogram",
-			"1    1    2    2","5    0    5    0    5" );
+    printf( "%s%11s%19s\n%54s\n%54s\n",
+            "Response", "Frequency", "Histogram",
+   	    "1    1    2    2","5    0    5    0    5" );
 	
-	for(rating=1; rating<=9; rating++)
-	{
-		printf("%8d%11d          ", rating, freq[ rating ]);
-		
-		if(freq[ rating ] > largest ) {
-			largest = freq[ rating ];
-		}
-		
-		for(h=1; h<=freq[ rating ]; h++)
-		    printf("*");
-		    
-		printf("\n");
+    for(rating=1; rating<=9; rating++)
+    {
+	printf("%8d%11d          ", rating, freq[ rating ]);
+	
+	if(freq[ rating ] > largest ) {
+   	    largest = freq[ rating ];
 	}
+		
+	for(h=1; h<=freq[ rating ]; h++)
+	    printf("*");
+		    
+	printf("\n");
+    }
 	
-	printf( "The mode is the most frequent value.\n" 
-	        "For this run the mode includes ( ");
+    printf( "The mode is the most frequent value.\n" 
+            "For this run the mode includes ( ");
 	
-	for(rating=1; rating<=9; rating++)
-	    if(freq[ rating ] == largest)
-	        printf("%d ", rating);
+    for(rating=1; rating<=9; rating++)
+        if(freq[ rating ] == largest)
+            printf("%d ", rating);
 	
 	
-	printf(") which occurred %d times.\n", largest );
+    printf(") which occurred %d times.\n", largest );
 }
 
 void bubbleSort( int a[] )
 {
-	int pass, j, hold;
+    int pass, j, hold;
 
     for(pass=1; pass<=SIZE-1; pass++)
-	{
-	    for(j=0; j<=SIZE-2; j++)
-	    {
-		    if(a[j] > a[j+1]) {
-		    	hold = a[j];
-		    	a[j] = a[j+1];
-		    	a[j+1]=hold;
-			}
+    {
+        for(j=0; j<=SIZE-2; j++)
+        {
+	    if(a[j] > a[j+1]) {
+	    	hold = a[j];
+	    	a[j] = a[j+1];
+	    	a[j+1]=hold;
 	    }
-	}	
+        }
+    }	
 }
 
 void printArray( const int a[] )
 {
-	int j;
+    int j;
 	
-	for(j=0; j<=SIZE-1; j++)
-	{
-		if( j%20==0 )
-		    printf("\n");
+    for(j=0; j<=SIZE-1; j++)
+    {
+	if( j%20==0 )
+	    printf("\n");
 		    
-		printf( "%2d", a[j] );
-	}
+    	printf( "%2d", a[j] );
+    }
 }
- 
