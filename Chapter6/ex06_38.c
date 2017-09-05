70d88b69ccf39f183c4bbb22238caa959b443d80
Andrew Laing <parisianconnections@gmail.com>
2017-09-05 11:45:44 +0100

    Update ex06_38.c

--- Chapter6/ex06_38.c
+++ Chapter6/ex06_38.c
@@ -1,5 +1,5 @@
 /*
- * Filename:	ex06_38.c
+ * Filename:    ex06_38.c
  * Author:      Andrew Laing
  * Email:       parisianconnections@gmail.com
  * Date:        21/05/2017
@@ -12,21 +12,21 @@ void stringReverse( char[]);
 
 int main()
 {
-	char str[] = "Test String 3";
-	
-	stringReverse( str );
-	
+    char str[] = "Test String 3";
+    
+    stringReverse( str );
+    
     return 0;
 }
 
 
 void stringReverse( char str[] )
 {
-	if(str[0]=='\0')
-	    return;
-	else
-		stringReverse(&str[1]); 
-	    
+    if(str[0]=='\0')
+        return;
+    else
+        stringReverse(&str[1]); 
+        
     printf("%c", str[0]); 
 }
 
