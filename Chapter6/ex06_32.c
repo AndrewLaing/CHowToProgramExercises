541ed3b1631c49bad24b1bba0cfe45fb87b74c8a
Andrew Laing <parisianconnections@gmail.com>
2017-09-05 11:43:30 +0100

    Update ex06_32.c

--- Chapter6/ex06_32.c
+++ Chapter6/ex06_32.c
@@ -1,5 +1,5 @@
  /*
- * Filename:	ex06_32.c
+ * Filename:    ex06_32.c
  * Author:      Andrew Laing
  * Email:       parisianconnections@gmail.com
  * Date:        20/05/2017
@@ -15,19 +15,19 @@ void selectionSort( int[], int );
 
 int main()
 {
-	int i;
-	int toSort[SIZE] = { 122, 642, 4, 322, 113, 9, 575, 44, 176, 8 };
-	
-	for(i=0; i<SIZE; i++)
-	    printf("%d ",toSort[i]);
-	printf("\n");
-	
+    int i;
+    int toSort[SIZE] = { 122, 642, 4, 322, 113, 9, 575, 44, 176, 8 };
+    
+    for(i=0; i<SIZE; i++)
+        printf("%d ",toSort[i]);
+    printf("\n");
+    
     selectionSort( toSort , SIZE  );
     
     for(i=0; i<SIZE; i++)
-	    printf("%d ",toSort[i]);
-	printf("\n");
-	
+        printf("%d ",toSort[i]);
+    printf("\n");
+    
     return 0;
 }
 
@@ -41,20 +41,20 @@ void selectionSort( int toSort[], int arrLen )
     int smallest = toSort[0];
 
     for(i=1; i<arrLen; i++) 
-	{
-    	if(toSort[i] < smallest) {
-    		smallest = toSort[i];
-    		index = i;
-		}
-	}
-	
-	if(smallest < toSort[0]) {
-		temp = toSort[0];
-		toSort[0] = smallest;
-		toSort[index] = temp;
-	}
-	
-	/* Note: pointer augmentation points to the next item in the array */
-	selectionSort( ++toSort, arrLen-1 );
+    {
+        if(toSort[i] < smallest) {
+            smallest = toSort[i];
+            index = i;
+        }
+    }
+    
+    if(smallest < toSort[0]) {
+        temp = toSort[0];
+        toSort[0] = smallest;
+        toSort[index] = temp;
+    }
+    
+    /* Note: pointer augmentation points to the next item in the array */
+    selectionSort( ++toSort, arrLen-1 );
 }
 
