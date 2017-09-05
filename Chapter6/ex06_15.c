b71f8f20c911e0886b780432c8e55493d433ca7b
Andrew Laing <parisianconnections@gmail.com>
2017-09-05 11:01:41 +0100

    Update ex06_15.c

--- Chapter6/ex06_15.c
+++ Chapter6/ex06_15.c
@@ -10,28 +10,27 @@
 
 int main()
 {
-	short input[SIZE] = {10,20,51,66,10,88,66,45,46,99,
-	                     33,55,45,68,12,11,45,65,77,44};
-	short alreadyRead[SIZE] = {0};
-	int nextIndex = 0;
-	int i, j, printNum;
+    short input[SIZE] = {10,20,51,66,10,88,66,45,46,99,
+                         33,55,45,68,12,11,45,65,77,44};
+    short alreadyRead[SIZE] = {0};
+    int nextIndex = 0;
+    int i, j, printNum;
 	
-	for(i=0; i<=SIZE-1; i++) {
-		printNum = 1;
+    for(i=0; i<=SIZE-1; i++) {
+	printNum = 1;
 		
-		for(j=0; j<nextIndex; j++) {
-			if(alreadyRead[j]==input[i]) {
-				printNum = 0;
-				break;
-			}
-		}
-		
-		if(printNum) {
-			printf("%d ", input[i]);
-			alreadyRead[nextIndex++] = input[i];
-		}
+	for(j=0; j<nextIndex; j++) {
+    	    if(alreadyRead[j]==input[i]) {
+		printNum = 0;
+		break;
+	    }
 	}
+		
+	if(printNum) {
+	    printf("%d ", input[i]);
+	    alreadyRead[nextIndex++] = input[i];
+	}
+    }
 	
     return 0;
 }
-
