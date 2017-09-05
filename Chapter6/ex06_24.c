e16aa638070b5e94d62e4acb040a5afa3e0d6790
Andrew Laing <parisianconnections@gmail.com>
2017-09-05 11:30:50 +0100

    Update ex06_24.c

--- Chapter6/ex06_24.c
+++ Chapter6/ex06_24.c
@@ -1,5 +1,5 @@
  /*
- * Filename:	ex06_24.c
+ * Filename:    ex06_24.c
  * Author:      Andrew Laing
  * Email:       parisianconnections@gmail.com
  * Date:        14/05/2017
@@ -20,14 +20,14 @@ const int vertical[SIZE]   = { -1, -2, -2, -1, 1, 2, 2, 1 };
 
 /* To help when choosing squares - low=higher priority */
 int heuristic[SIZE][SIZE] = {
-	{2,3,4,4,4,4,3,2},
-	{3,4,6,6,6,6,4,3},
-	{4,6,8,8,8,8,6,4},
-	{4,6,8,8,8,8,6,4},
-	{4,6,8,8,8,8,6,4},
-	{4,6,8,8,8,8,6,4},
-	{3,4,6,6,6,6,4,3},
-	{2,3,4,4,4,4,3,2}
+    {2,3,4,4,4,4,3,2},
+    {3,4,6,6,6,6,4,3},
+    {4,6,8,8,8,8,6,4},
+    {4,6,8,8,8,8,6,4},
+    {4,6,8,8,8,8,6,4},
+    {4,6,8,8,8,8,6,4},
+    {3,4,6,6,6,6,4,3},
+    {2,3,4,4,4,4,3,2}
 };
 
 
@@ -40,50 +40,50 @@ int isValidMove( int, int, int );
 
 int getRandomNum(int max)
 {
-	return rand()%max;
+    return rand()%max;
 }
 
 
 void printBoard(int currentRow, int currentColumn) 
 {
-	int h, v;
-	for(v=0; v<SIZE; v++)
-	{
-		for(h=0; h<SIZE; h++){
-			if( v==currentRow && h==currentColumn )
-			    printf("$ ");
-			else if( board[v][h]==1 )
-			    printf("x ");
-			else
-			    printf(": ");
-		}
-		printf("\n");
-	}
+    int h, v;
+    for(v=0; v<SIZE; v++)
+    {
+        for(h=0; h<SIZE; h++){
+            if( v==currentRow && h==currentColumn )
+                printf("$ ");
+            else if( board[v][h]==1 )
+                printf("x ");
+            else
+                printf(": ");
+        }
+        printf("\n");
+    }
 }
 
 /*  Return 1 if move is a valid move from the current position
  *  otherwise return 0 */
 int isValidMove(int currentRow, int currentColumn, int move)
 {
-	currentRow    += vertical[move];
-	currentColumn += horizontal[move];
-	
-	if(currentRow<0 || currentRow>7 ||
-	   currentColumn<0 || currentColumn>7 ||
-	   board[currentRow][currentColumn]==1)
-	   return 0;
-	else 
-	    return 1;
+    currentRow    += vertical[move];
+    currentColumn += horizontal[move];
+    
+    if(currentRow<0 || currentRow>7 ||
+       currentColumn<0 || currentColumn>7 ||
+       board[currentRow][currentColumn]==1)
+       return 0;
+    else 
+        return 1;
 }
 
 
 int main()
 {
-	srand(time( NULL ));
-	
-	int squaresVisited = 1;
+    srand(time( NULL ));
+    
+    int squaresVisited = 1;
     int currentRow = getRandomNum(SIZE); 
-	int currentColumn = getRandomNum(SIZE);      
+    int currentColumn = getRandomNum(SIZE);      
     
     board[currentRow][currentColumn]=1;
 
@@ -97,33 +97,32 @@ int main()
     
     while( movesAvailable )
     {
-    	movesAvailable=0;
-    	
-	    for(move=0; move<8; move++)
-	    {
-	    	if(isValidMove( currentRow, currentColumn, move ) ) {
-	    		possibleMoves[movesAvailable]=move;
-	    		movesAvailable++;
-			}  
-		}
+        movesAvailable=0;
+        
+        for(move=0; move<8; move++)
+        {
+            if(isValidMove( currentRow, currentColumn, move ) ) {
+                possibleMoves[movesAvailable]=move;
+                movesAvailable++;
+            }  
+        }
 
-	    getchar();
-	    
-	    if(movesAvailable)
-	    {
-		    choice = possibleMoves[ getRandomNum(movesAvailable) ];
-			currentRow    += vertical[choice];
-			currentColumn += horizontal[choice];
-			board[currentRow][currentColumn] = 1;
-			squaresVisited++;
-			system("cls");    
-		    printBoard(currentRow, currentColumn); 	
-			printf("Squares visited = %d\n", squaresVisited);    	
-		}
-	}    
+        getchar();
+        
+        if(movesAvailable)
+        {
+            choice = possibleMoves[ getRandomNum(movesAvailable) ];
+            currentRow    += vertical[choice];
+            currentColumn += horizontal[choice];
+            board[currentRow][currentColumn] = 1;
+            squaresVisited++;
+            system("cls");    
+            printBoard(currentRow, currentColumn);  
+            printf("Squares visited = %d\n", squaresVisited);       
+        }
+    }    
 
     printf("\nNo more moves possible.\n");
     
     return 0;
 }
-
