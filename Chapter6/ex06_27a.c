5c8e1ceb8bef668e54ffeabad6f3fe4baca8800e
Andrew Laing <parisianconnections@gmail.com>
2017-09-05 11:38:20 +0100

    Update ex06_27a.c

--- Chapter6/ex06_27a.c
+++ Chapter6/ex06_27a.c
@@ -1,5 +1,5 @@
  /*
- * Filename:	ex06_27a.c
+ * Filename:    ex06_27a.c
  * Author:      Andrew Laing
  * Email:       parisianconnections@gmail.com
  * Date:        16/05/2017
@@ -20,14 +20,14 @@ int availableSquares[SIZE][SIZE];
 
 /* To help when choosing squares - low=higher priority */
 int heuristic[SIZE][SIZE] = {
-	{22,22,22,22,22,22,22,22},
-	{22,24,24,24,24,24,24,22},
-	{22,24,26,26,26,26,24,22},
-	{22,24,26,28,28,26,24,22},
-	{22,24,26,28,28,26,24,22},
-	{22,24,26,26,26,26,24,22},
-	{22,24,24,24,24,24,24,22},
-	{22,22,22,22,22,22,22,22}
+    {22,22,22,22,22,22,22,22},
+    {22,24,24,24,24,24,24,22},
+    {22,24,26,26,26,26,24,22},
+    {22,24,26,28,28,26,24,22},
+    {22,24,26,28,28,26,24,22},
+    {22,24,26,26,26,26,24,22},
+    {22,24,24,24,24,24,24,22},
+    {22,22,22,22,22,22,22,22}
 };
 
 
@@ -36,93 +36,93 @@ int getRandomNum( int );
 
 int getRandomNum(int max)
 {
-	return rand()%max;
+    return rand()%max;
 }
 
 
 void printBoard() 
 {
-	int h, v;
-	for(v=0; v<SIZE; v++)
-	{
-		for(h=0; h<SIZE; h++){
-			if( board[v][h]==1 )
-			    printf("@ ");
-			else
-			    printf(": ");
-		}
-		printf("\n");
-	}
+    int h, v;
+    for(v=0; v<SIZE; v++)
+    {
+        for(h=0; h<SIZE; h++){
+            if( board[v][h]==1 )
+                printf("@ ");
+            else
+                printf(": ");
+        }
+        printf("\n");
+    }
 }
 
 
 void printHeuristic() 
 {
-	int h, v;
-	for(v=0; v<SIZE; v++)
-	{
-		for(h=0; h<SIZE; h++){
-			printf("%2d ", heuristic[h][v]);
-		}
-		printf("\n");
-	}
+    int h, v;
+    for(v=0; v<SIZE; v++)
+    {
+        for(h=0; h<SIZE; h++){
+            printf("%2d ", heuristic[h][v]);
+        }
+        printf("\n");
+    }
 }
 
 
 /*  Return 1 if move is a valid move otherwise return 0 */
 int isValidMove(int row, int col)
 {
-	return availableSquares[row][col];
+    return availableSquares[row][col];
 }
 
 
 /* tested and working */
 void makeMove(int row, int col)
 {
-	board[row][col]=1;
-	
-	// update availableSquares array
-	int i,j;
-	for(i=0; i<SIZE; i++)
-	{
-		availableSquares[row][i]=0;
-		availableSquares[i][col]=0;
-	}
-	
-	// update diagonally up left
-	j = col;
-	for(i=row; i>=0 && j>=0; i--, j--)
-	    availableSquares[i][j]=0;
-	    
-	// update diagonally down left
-	j = col;
-	for(i=row; i<SIZE && j>=0; i++, j--)
-	    availableSquares[i][j]=0;
-	    
-	// update diagonally down right
-	j = col;
-	for(i=row; i<SIZE && j<SIZE; i++, j++)
-	    availableSquares[i][j]=0;
-	    
-	// update diagonally up right
-	j = col;
-	for(i=row; i>=0 && j<SIZE; i--, j++)
-	    availableSquares[i][j]=0;
+    board[row][col]=1;
+    
+    // update availableSquares array
+    int i,j;
+    for(i=0; i<SIZE; i++)
+    {
+        availableSquares[row][i]=0;
+        availableSquares[i][col]=0;
+    }
+    
+    // update diagonally up left
+    j = col;
+    for(i=row; i>=0 && j>=0; i--, j--)
+        availableSquares[i][j]=0;
+        
+    // update diagonally down left
+    j = col;
+    for(i=row; i<SIZE && j>=0; i++, j--)
+        availableSquares[i][j]=0;
+        
+    // update diagonally down right
+    j = col;
+    for(i=row; i<SIZE && j<SIZE; i++, j++)
+        availableSquares[i][j]=0;
+        
+    // update diagonally up right
+    j = col;
+    for(i=row; i>=0 && j<SIZE; i--, j++)
+        availableSquares[i][j]=0;
 }
 
 
 void initialiseBoard()
 {
-	int i, j;
+    int i, j;
     
     /* Initialise board state */
     for(i=0; i<SIZE; i++)
     {
-    	for(j=0; j<SIZE; j++) {
-    		board[i][j] = 0;
-    		availableSquares[i][j] = 1;
-		}	    
-	}
+        for(j=0; j<SIZE; j++) {
+            board[i][j] = 0;
+            availableSquares[i][j] = 1;
+        }       
+    }
 }
 
 
@@ -140,35 +140,32 @@ int main()
     
     for(y=0; y<2000; y++)
     {
-    	queens = 0;
-    	initialiseBoard();
-    	
-	    for(choiceRow=0; choiceRow<SIZE; choiceRow++)
-	    {
-	    	for(x=0;x<1000;x++)
-	    	{
-	    		choiceCol = getRandomNum(SIZE);
-				if( isValidMove(choiceRow, choiceCol) ) {
-					makeMove(choiceRow, choiceCol);
-					queens++;
-					break;
-				}
-			}
-			if(x >= 1000)
-			    break;
-			
-//			system("cls");
-//			printBoard(); 
-		} 
-		
-		system("cls");
-		printBoard(); 
-		
-		if(queens==8)
-			solved++;
-			
-		printf("\nSolved %d times\n", solved);	
-	}
+        queens = 0;
+        initialiseBoard();
+        
+        for(choiceRow=0; choiceRow<SIZE; choiceRow++)
+        {
+            for(x=0;x<1000;x++)
+            {
+                choiceCol = getRandomNum(SIZE);
+                if( isValidMove(choiceRow, choiceCol) ) {
+                    makeMove(choiceRow, choiceCol);
+                    queens++;
+                    break;
+                }
+            }
+            if(x >= 1000)
+                break;
+        } 
+        
+        system("cls");
+        printBoard(); 
+        
+        if(queens==8)
+            solved++;
+            
+        printf("\nSolved %d times\n", solved);  
+    }
     
     return 0;
 }
