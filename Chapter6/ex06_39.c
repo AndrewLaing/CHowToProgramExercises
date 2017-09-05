f659731bf638e98e985f2377c6fff1490c1a36f4
Andrew Laing <parisianconnections@gmail.com>
2017-09-05 11:46:14 +0100

    Update ex06_39.c

--- Chapter6/ex06_39.c
+++ Chapter6/ex06_39.c
@@ -1,5 +1,5 @@
 /*
- * Filename:	ex06_39.c
+ * Filename:    ex06_39.c
  * Author:      Andrew Laing
  * Email:       parisianconnections@gmail.com
  * Date:        21/05/2017
@@ -12,22 +12,22 @@ int minimum( int[], int );
 
 int main()
 {
-	int arrLen = 10;
-	int array[10] = {27,5,3,6,44,15,5,7,25,14};
-		    
+    int arrLen = 10;
+    int array[10] = {27,5,3,6,44,15,5,7,25,14};
+            
     printf("Minimum = %d\n", minimum(array, arrLen)); 
-	
+    
     return 0;
 }
 
 
 int minimum( int array[], int arrLen )
 {
-	if(arrLen == 1)
-	    return array[0];
-	else if(array[0] < array[arrLen-1])
-		return minimum(array, arrLen-1);
-	else
-	    return minimum(++array, arrLen-1); 
+    if(arrLen == 1)
+        return array[0];
+    else if(array[0] < array[arrLen-1])
+        return minimum(array, arrLen-1);
+    else
+        return minimum(++array, arrLen-1); 
 }
 
