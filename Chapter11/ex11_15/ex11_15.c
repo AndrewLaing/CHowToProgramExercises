be4f31eb5ad25b135f7fcd36eb58b0c87abdb148
Andrew Laing <parisianconnections@gmail.com>
2017-09-05 15:35:08 +0100

    Add files via upload

--- Chapter11/ex11_15/ex11_15.c
+++ Chapter11/ex11_15/ex11_15.c
@@ -1,5 +1,5 @@
 /*
- * Filename:	ex11_15.c
+ * Filename:    ex11_15.c
  * Author:      Andrew Laing
  * Email:       parisianconnections@gmail.com
  * Date:        30/07/2017.
@@ -8,7 +8,7 @@
 #include <stdio.h>
 
 int main(int argc, char **argv)
-{	   
+{      
     FILE *ifPtr = stdin;
     FILE *ofPtr = stdout;
 
@@ -21,46 +21,46 @@ int main(int argc, char **argv)
     if(argc < 2)
         fputs( "Enter a line of text: ", ofPtr );
     else if(argc > 1) {
-	    if ( ( ifPtr = fopen( argv[1], "r" ) ) == NULL )
-	    {
-	    	printf("Error - unable to open file \"%s\"\n", argv[1]);
-	    	return -1;
-		}
-		
-	    if(argc > 2) {
-		    if ( ( ofPtr = fopen( argv[2], "w" ) ) == NULL )
-		    {
-		    	printf("Error - unable to open file \"%s\"\n", argv[2]);
-		    	return -1;
-			}
-    	}	
-	}
+        if ( ( ifPtr = fopen( argv[1], "r" ) ) == NULL )
+        {
+            printf("Error - unable to open file \"%s\"\n", argv[1]);
+            return -1;
+        }
+        
+        if(argc > 2) {
+            if ( ( ofPtr = fopen( argv[2], "w" ) ) == NULL )
+            {
+                printf("Error - unable to open file \"%s\"\n", argv[2]);
+                return -1;
+            }
+        }   
+    }
     
     if(argc < 2) 
-	{
-    	while(( c=fgetc(ifPtr) ) != '\n')
+    {
+        while(( c=fgetc(ifPtr) ) != '\n')
             sentence[i++] = c;
             
         sentence[i] = '\0';
-	    
-		fputs( "\nThe line entered was: ", ofPtr );
-	    fputs( sentence, ofPtr );        
-	}
+        
+        fputs( "\nThe line entered was: ", ofPtr );
+        fputs( sentence, ofPtr );        
+    }
     else 
-	{
-    	while(( c=fgetc(ifPtr) ) != EOF) {
-    		sentence[i++] = c;
-    		if(c=='\n') {
-    			sentence[i] = '\0';
-    			i = 0;
-	            fputs( sentence, ofPtr ); 
-			}
-		}
-		sentence[i] = '\0';
+    {
+        while(( c=fgetc(ifPtr) ) != EOF) {
+            sentence[i++] = c;
+            if(c=='\n') {
+                sentence[i] = '\0';
+                i = 0;
+                fputs( sentence, ofPtr ); 
+            }
+        }
+        sentence[i] = '\0';
         fputs( sentence, ofPtr ); 
-	}
+    }
 
- 	return 0;
+    return 0;
 }
 
 
