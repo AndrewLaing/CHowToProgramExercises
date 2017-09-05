44d376f22f401dbac4a8061015a5f34e3797ea15
Andrew Laing <parisianconnections@gmail.com>
2017-09-05 11:42:06 +0100

    Update ex06_30.c

--- Chapter6/ex06_30.c
+++ Chapter6/ex06_30.c
@@ -1,5 +1,5 @@
  /*
- * Filename:	ex06_30.c
+ * Filename:    ex06_30.c
  * Author:      Andrew Laing
  * Email:       parisianconnections@gmail.com
  * Date:        16/05/2017
@@ -12,25 +12,25 @@
 
 int main()
 {
-	int array[ SIZE ];
-	int i, j;
-	
-	for(i=0; i<SIZE; i++)
+    int array[ SIZE ];
+    int i, j;
+    
+    for(i=0; i<SIZE; i++)
         array[i]=1;
     
     for(i=2; i<SIZE; i++) {
-    	for(j=i+1; j<SIZE; j++) {
-    		if(array[j]==1 && j%i==0)
-    		    array[j]=0;
-		}
-	}
-	
-	for(i=0; i<SIZE; i++)
+        for(j=i+1; j<SIZE; j++) {
+            if(array[j]==1 && j%i==0)
+                array[j]=0;
+        }
+    }
+    
+    for(i=0; i<SIZE; i++)
         if(array[i]==1)
             printf("%d ", i);
             
     printf("\n");
-	
+    
     return 0;
 }
 
