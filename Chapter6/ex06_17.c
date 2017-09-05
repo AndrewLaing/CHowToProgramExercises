fc65512b396f931f3fa29ab9b881b8a533385399
Andrew Laing <parisianconnections@gmail.com>
2017-09-05 11:02:31 +0100

    Update ex06_17.c

--- Chapter6/ex06_17.c
+++ Chapter6/ex06_17.c
@@ -13,19 +13,19 @@ int whatIsThis( const int[], int );
 
 int main()
 {
-	int x, a[ SIZE ] = {1,2,3,4,5,6,7,8,9,10};
+    int x, a[ SIZE ] = {1,2,3,4,5,6,7,8,9,10};
 	
-	x = whatIsThis( a, SIZE );
-	printf("Result is %d\n", x);
+    x = whatIsThis( a, SIZE );
+    printf("Result is %d\n", x);
 	
     return 0;
 }
 
+
 int whatIsThis( const int b[], int p )
 {
-	if(p==1)
+    if(p==1)
     	return b[0];
     else
         return b[p-1] + whatIsThis(b, p-1); 
 }
- 
