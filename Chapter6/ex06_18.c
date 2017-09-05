dedc730b04533abc359f7be9847aa4a1084828f6
Andrew Laing <parisianconnections@gmail.com>
2017-09-05 11:03:37 +0100

    Update ex06_18.c

--- Chapter6/ex06_18.c
+++ Chapter6/ex06_18.c
@@ -12,21 +12,20 @@ void someFunction( const int[], int );
 
 int main()
 {
-	int a[ SIZE ] = {8,3,1,2,6,0,9,7,4,5};
+    int a[ SIZE ] = {8,3,1,2,6,0,9,7,4,5};
 	
-	printf("Answer is:\n");
-	someFunction( a, SIZE );
-	printf("\n"); 
+    printf("Answer is:\n");
+    someFunction( a, SIZE );
+    printf("\n"); 
 	
     return 0;
 }
 
 void someFunction( const int b[], int size )
 {
-	 if( size > 0)
-	 {
-	 	someFunction( &b[ 1 ], size-1 );
-		 printf("%d  ", b[ 0 ]); 
-	 }
+    if( size > 0)
+    {
+	someFunction( &b[ 1 ], size-1 );
+        printf("%d  ", b[ 0 ]); 
+    }
 }
- 
