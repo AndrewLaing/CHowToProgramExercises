35bb59a84af27c10b1c6d02bde6cd5eb79c20233
Andrew Laing <parisianconnections@gmail.com>
2017-09-05 10:50:02 +0100

    Update ex06_19.c

--- Chapter6/ex06_19.c
+++ Chapter6/ex06_19.c
@@ -16,22 +16,22 @@ int getRollResult(  );
 
 int getRollResult()
 {
-	return 1 + rand()%SIDES;
+    return 1 + rand()%SIDES;
 }
 
 
 int main()
 {
-	srand( (time(NULL)) );
-	int totals[13] = {0};
-	int i;
+    srand( (time(NULL)) );
+    int totals[13] = {0};
+    int i;
 	
-	for(i=0; i<=36000; i++)
-	   ++totals[( getRollResult() + getRollResult() )];
+    for(i=0; i<=36000; i++)
+        ++totals[( getRollResult() + getRollResult() )];
 	
     for(i=2; i<=12; i++) {
     	printf("%d:\t%d\n", i, totals[i]);
-	}
+    }
 	
     return 0;
 }
